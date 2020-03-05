e8a875b402d6bdf64b9a9590836b514bcaca54ef
吴贻韡 <mizuka.wu@outlook.com>
2020-03-05 15:57:02 +0800

    feat: 增加外层容器

--- docs/.vuepress/components/Seed.vue
+++ docs/.vuepress/components/Seed.vue
@@ -15,6 +15,10 @@ export default {
 <style lang="stylus">
 // 修复组件内因为外部主题导致的样式问题
 .seed-demo
+  padding 10px
+  border 1px solid #ddd
+  margin 10px 0
+  border-radius 4px
   table
     margin initial
   th, td

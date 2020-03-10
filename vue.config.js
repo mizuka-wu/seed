4042446c51200fa9987bfb8cb6d7e79f81e62ffd
吴贻韡 <mizuka.wu@outlook.com>
2020-03-10 19:36:42 +0800

    feat: 减少element-ui依赖

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,19 @@ const path = require("path");
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
+
 module.exports = {
   chainWebpack: config => {
     config.resolve.alias.set("@", resolve("examples"));
+    if (process.env.NODE_ENV === "production") {
+      config.externals({
+        "element-ui": {
+          commonjs: "element-ui",
+          commonjs2: "element-ui",
+          root: "ELEMENT"
+        }
+      });
+    }
   },
   pages: {
     index: {

3006bbedbfde8afda97c471f564c809ee2b58308
吴贻韡 <mizuka.wu@outlook.com>
2020-07-30 23:11:11 +0800

    feat: 新的component方法

--- examples/App.vue
+++ examples/App.vue
@@ -1,84 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <Seed :fetchList="fetchList" :seeds="seeds" :addItem="addItem" sortable>
-      <template #batchList="{ rows }">
-        <el-button size="small" type="danger"
-          >{{ rows.length }}已选择</el-button
-        >
-      </template>
-      <template #control="scope">
-        <el-button>审核{{ scope.row }}</el-button>
-      </template>
-      <template #tools>
-        hello
-      </template>
-    </Seed>
+    <component v-if="currentExample" :is="currentExample" />
   </div>
 </template>
 
 <script>
-import Seed from "../packages/SeedContainer.vue";
-import resource from "./mock/resource";
+const exampleContext = require.context("./", false, /\.vue/);
+const Examples = exampleContext
+  .keys()
+  .filter(item => item !== "./App.vue")
+  .reduce((_examples, example) => {
+    const component = exampleContext(example);
+    const componentName = example.replace(/\.\/(.*)\.vue/, "$1");
+    return (_examples[componentName] = component);
+  }, {});
 
 export default {
   name: "app",
   components: {
-    Seed
+    ...Examples
+  },
+  props: {
+    example: {
+      type: String
+    }
   },
   data() {
     return {
-      seeds: [
-        {
-          key: "name",
-          label: "名字",
-          render(h, value) {
-            return `"${value}"`;
-          },
-          options: {
-            required: true
-          }
-        },
-        {
-          key: "address",
-          label: "地址"
-        },
-        {
-          key: "pic"
-        },
-        {
-          key: "description"
-        },
-        {
-          key: "blog"
-        },
-        {
-          key: "ip"
-        },
-        {
-          key: "birthday"
-        },
-        {
-          key: "addtime"
-        },
-        {
-          key: "updatetime"
-        }
-      ]
+      examples: Object.keys(Examples)
     };
   },
-  methods: {
-    fetchList(params) {
-      return resource.getList(params);
-    },
-    addItem(row) {
-      return resource.addItem(row);
-    },
-    updateItem(row) {
-      return resource.updateItem(row);
-    },
-    deleteItem(row) {
-      return resource.deleteItem(row);
+  computed: {
+    currentExample({ example, examples }) {
+      if (examples.includes(example)) {
+        return example;
+      } else {
+        return examples[0];
+      }
     }
   }
 };
---
+++ examples/BaseExample.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <Seed :fetchList="fetchList" :seeds="seeds" :addItem="addItem" sortable>
+      <template #batchList="{ rows }">
+        <el-button size="small" type="danger"
+          >{{ rows.length }}已选择</el-button
+        >
+      </template>
+      <template #control="scope">
+        <el-button>审核{{ scope.row }}</el-button>
+      </template>
+      <template #tools>
+        hello
+      </template>
+    </Seed>
+  </div>
+</template>
+
+<script>
+import Seed from "../packages/SeedContainer.vue";
+import resource from "./mock/resource";
+
+export default {
+  name: "app",
+  components: {
+    Seed
+  },
+  data() {
+    return {
+      seeds: [
+        {
+          key: "name",
+          label: "名字",
+          render(h, value) {
+            return `"${value}"`;
+          },
+          options: {
+            required: true
+          }
+        },
+        {
+          key: "address",
+          label: "地址"
+        },
+        {
+          key: "pic"
+        },
+        {
+          key: "description"
+        },
+        {
+          key: "blog"
+        },
+        {
+          key: "ip"
+        },
+        {
+          key: "birthday"
+        },
+        {
+          key: "addtime"
+        },
+        {
+          key: "updatetime"
+        }
+      ]
+    };
+  },
+  methods: {
+    fetchList(params) {
+      return resource.getList(params);
+    },
+    addItem(row) {
+      return resource.addItem(row);
+    },
+    updateItem(row) {
+      return resource.updateItem(row);
+    },
+    deleteItem(row) {
+      return resource.deleteItem(row);
+    }
+  }
+};
+</script>

b7d1431274c6d555249ac33c9ce4e165ed863d09
吴贻韡 <mizuka.wu@outlook.com>
2020-07-31 00:00:13 +0800

    fix: 移除fullscreen依赖

--- docs/.vuepress/components/SeedExample.vue
+++ docs/.vuepress/components/SeedExample.vue
@@ -1,72 +1,69 @@
 <template>
   <ClientOnly>
     <div class="seed-demo" v-loading="loadingSourceCode">
-      <fullscreen
-        ref="fullscreen"
-        background="#ffffff"
-        @change="e => (isFullscreen = e)"
+      <div class="control">
+        <el-tooltip
+          effect="dark"
+          :content="isShowSourceCode ? '返回' : '查看源代码'"
+          placement="bottom"
+        >
+          <el-button
+            size="large"
+            type="text"
+            icon="el-icon-document"
+            @click="handleShowSourceCode(!isShowSourceCode)"
+          />
+        </el-tooltip>
+        <el-tooltip
+          v-if="isShowSourceCode"
+          effect="dark"
+          content="复制当前代码"
+          placement="bottom"
+        >
+          <el-button
+            size="large"
+            type="text"
+            v-clipboard:copy="sourceCodeRaw"
+            v-clipboard:success="handleCopySourceCode"
+            icon="el-icon-copy-document"
+          />
+        </el-tooltip>
+        <el-tooltip
+          effect="dark"
+          :content="isFullscreen ? '退出全屏' : '全屏'"
+          placement="bottom"
+        >
+          <el-button
+            size="large"
+            type="text"
+            icon="el-icon-full-screen"
+            @click="handleFullscreen"
+          />
+        </el-tooltip>
+      </div>
+      <div v-if="isShowSourceCode" class="source-code">
+        <div class="language-vue extra-class">
+          <pre
+            class="language-vue"
+          ><code ref="code" v-html="sourceCode" /></pre>
+        </div>
+      </div>
+      <component
+        :is="isFullscreen ? 'el-dialog' : 'div'"
+        :visible.sync="isFullscreen"
+        fullscreen
+        v-else
       >
-        <div class="control">
-          <el-tooltip
-            effect="dark"
-            :content="isShowSourceCode ? '返回' : '查看源代码'"
-            placement="bottom"
-          >
-            <el-button
-              size="large"
-              type="text"
-              icon="el-icon-document"
-              @click="handleShowSourceCode(!isShowSourceCode)"
-            />
-          </el-tooltip>
-          <el-tooltip
-            v-if="isShowSourceCode"
-            effect="dark"
-            content="复制当前代码"
-            placement="bottom"
-          >
-            <el-button
-              size="large"
-              type="text"
-              v-clipboard:copy="sourceCodeRaw"
-              v-clipboard:success="handleCopySourceCode"
-              icon="el-icon-copy-document"
-            />
-          </el-tooltip>
-          <el-tooltip
-            effect="dark"
-            :content="isFullscreen ? '退出全屏' : '全屏'"
-            placement="bottom"
-          >
-            <el-button
-              size="large"
-              type="text"
-              icon="el-icon-full-screen"
-              @click="handleFullscreen"
-            />
-          </el-tooltip>
-        </div>
-        <div v-if="isShowSourceCode" class="source-code">
-          <div class="language-vue extra-class">
-            <pre
-              class="language-vue"
-            ><code ref="code" v-html="sourceCode" /></pre>
-          </div>
-        </div>
-        <App :example="example" v-else />
-      </fullscreen>
+        <App :example="example" />
+      </component>
     </div>
   </ClientOnly>
 </template>
 
 <script>
-import fullscreen from "vue-fullscreen";
-import Vue from "vue";
 import App from "../../../examples/App";
 import resource from "../../../examples/mock/resource";
 import prism from "prismjs";
-
-Vue.use(fullscreen);
 
 export default {
   components: {
@@ -88,7 +85,7 @@ export default {
   },
   methods: {
     handleFullscreen() {
-      this.$refs.fullscreen.toggle();
+      this.isFullscreen = !this.isFullscreen;
     },
     handleShowSourceCode(e) {
       const vm = this;
@@ -132,6 +129,7 @@ export default {
     padding 0 10px
     display flex
     flex-direction row-reverse
+    margin-bottom -10px
   .source-code
     padding 10px
   table

8124c3491a42fcac8205576ff92dc5b9b914ea15
Edward Mizuka <mizuka.wu@outlook.com>
2020-08-04 11:56:39 +0800

    Update enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,12 @@
 import Element from "element-ui";
 import VueClipboard from "vue-clipboard2";
 import "element-ui/lib/theme-chalk/index.css";
+import Seed from "#/index";
 
 export default ({ Vue }) => {
   VueClipboard.config.autoSetContainer = true;
-
+  
+  Vue.use(Seed);
   Vue.use(Element);
   Vue.use(VueClipboard);
 };

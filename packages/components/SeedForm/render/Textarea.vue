7a068e67c7bdba31df791cb9b54ceddb2c1c1770
吴贻韡 <mizuka.wu@outlook.com>
2020-08-17 00:22:27 +0800

    feat: textarea

---
+++ packages/components/SeedForm/render/Textarea.vue
@@ -0,0 +1,34 @@
+<!-- @format -->
+
+<template>
+  <el-input v-model="data" type="textarea" v-bind="seed.options"></el-input>
+</template>
+
+<script>
+/** @format */
+export default {
+  props: {
+    seed: {
+      type: Object,
+      required: true
+    },
+    value: {
+      type: String
+    }
+  },
+  computed: {
+    data: {
+      get() {
+        return this.value || "";
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+/** @format */
+</style>

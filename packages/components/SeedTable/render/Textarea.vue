5b6fdea56c304f3d0d6878542b81a3b5da7faa35
吴贻韡 <mizuka.wu@outlook.com>
2020-08-17 00:17:04 +0800

    feat: 超大文本

--- packages/components/SeedTable/render/Textarea.vue
+++ packages/components/SeedTable/render/Textarea.vue
@@ -4,15 +4,32 @@
   <el-popover
     placement="top-start"
     trigger="hover"
-    width="300"
-    :disabled="!showClamp"
+    :width="popoverWidth"
+    :disabled="!showClamp || isLargeContext"
   >
     <div class="popover" v-html="value"></div>
     <div slot="reference">
       <div :style="{ height: `${line * 1.5}em`, overflow: 'hidden' }">
         <div v-html="value" ref="reference"></div>
       </div>
-      <span v-if="showClamp">...</span>
+      <!-- <div v-if="showClamp">
+        ...
+        <template v-if="isLargeContext">
+          <el-dialog
+            width="80%"
+            v-if="isLargeContext"
+            :visible.sync="isShowDetail"
+          >
+            <div v-html="value"></div>
+          </el-dialog>
+          <el-button
+            style="float: right;"
+            type="text"
+            @click="isShowDetail = true"
+            >查看详情</el-button
+          >
+        </template>
+      </div> -->
     </div>
   </el-popover>
 </template>
@@ -22,10 +39,14 @@ export default {
   props: ["value", "seed"],
   data() {
     return {
+      popoverWidth: 300,
       showClamp: false,
-      line: 3
+      line: 3,
+      isShowDetail: false,
+      isLargeContext: false
     };
   },
+  methods: {},
   mounted() {
     const { options = {} } = this.seed;
     const { line = 3 } = options;
@@ -33,8 +54,13 @@ export default {
     this.$nextTick(() => {
       const valueContainer = this.$refs.reference;
       if (valueContainer) {
+        // 大于容器则显示...
         this.showClamp =
           valueContainer.clientHeight > valueContainer.parentNode.clientHeight;
+
+        // // 判断是否是超大文本, 大于70%的页面高度
+        // this.isLargeContext =
+        //   valueContainer.clientHeight > document.body.offsetHeight * 0.7;
       }
     });
   }

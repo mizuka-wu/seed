d60954004d44d989f30f123a55852daff653dc40
吴贻韡 <mizuka.wu@outlook.com>
2020-08-20 18:01:15 +0800

    feat: 基本

--- examples/BaseExample.vue
+++ examples/BaseExample.vue
@@ -1,29 +1,6 @@
 <template>
   <div id="app">
-    <SeedPagingTable
-      :fetchList="fetchList"
-      :seeds="seeds"
-      :addItem="addItem"
-      :deleteItem="deleteItem"
-      :updateItem="updateItem"
-      sortable
-      excel
-    >
-      <template #batchList="{ rows }">
-        <el-button size="small" type="danger"
-          >{{ rows.length }}已选择</el-button
-        >
-      </template>
-      <template slot="control" slot-scope="scope">
-        <el-button>审核{{ scope.row.name }}</el-button>
-      </template>
-      <template #table:blogRender="{ value }"> {{ value }} hello </template>
-      <template #filter:blogRender="{ value }"> {{ value }} hello </template>
-
-      <template>
-        hello
-      </template>
-    </SeedPagingTable>
+    <Seed :fetchList="fetchList" :seeds="seeds" />
   </div>
 </template>
 
@@ -43,54 +20,12 @@ export default {
           }
         },
         {
-          key: "address",
-          label: "地址",
-          options: {
-            table: {
-              render(h, { props }) {
-                return props.value + "11112121212";
-              }
-            }
-          }
-        },
-        {
-          key: "pic",
-          label: "图片",
-          render: "image"
-        },
-        {
-          key: "video",
-          label: "视频",
-          render: "video",
-          options: {}
-        },
-        {
-          key: "description",
-          render: "textarea"
-        },
-        {
-          key: "blog"
-        },
-        {
-          key: "ip"
-        },
-        {
           key: "status",
           render: "enum",
+          label: "状态",
           options: {
             enum: ENUM
           }
-        },
-        {
-          key: "birthday"
-        },
-        {
-          key: "addtime",
-          render: "time"
-        },
-        {
-          key: "updatetime",
-          render: "time"
         }
       ]
     };
@@ -98,15 +33,6 @@ export default {
   methods: {
     fetchList(params) {
       return resource.getList(params);
-    },
-    addItem(row) {
-      return resource.addItem(row);
-    },
-    updateItem(row) {
-      return resource.updateItem(row);
-    },
-    deleteItem(row) {
-      return resource.deleteItem(row);
     }
   }
 };
